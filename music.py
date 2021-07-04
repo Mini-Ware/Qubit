c33139105bfbd4d9f77163bb1f0b9c3ef7f595aa
Mini Ware <codauknow@gmail.com>
2021-07-04 18:48:57 +0800

    Update music.py

--- music.py
+++ music.py
@@ -7,10 +7,16 @@ client = commands.Bot(command_prefix="q!")
 @client.command()
 async def play(ctx):
     voiceChannel = ctx.author.voice.channel
-    await voiceChannel.connect()
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    voice.play(discord.FFmpegPCMAudio("song.m4a"))
-    await ctx.send("Playing file")
+    if voice.is_connected():
+        await ctx.send("Already connected to a voice channel")
+    else:
+        await voiceChannel.connect()
+    if voice.is_playing():
+      await ctx.send("Music is currently playing")
+    else:
+      voice.play(discord.FFmpegPCMAudio("radio/song.m4a"))
+      await ctx.send("Playing music")
 
 @client.command()
 async def leave(ctx):
@@ -18,34 +24,8 @@ async def leave(ctx):
     if voice.is_connected():
         await voice.disconnect()
     else:
-        await ctx.send("The bot is not connected to a voice channel.")
+        await ctx.send("Not connected to a voice channel")
     await ctx.send("Leaving")
-
-@client.command()
-async def pause(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_playing():
-        voice.pause()
-    else:
-        await ctx.send("Currently no audio is playing.")
-    await ctx.send("Stopped")
-
-@client.command()
-async def resume(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_paused():
-        voice.resume()
-        await ctx.send("Continue")
-    else:
-        await ctx.send("Not paused")
-
-
-@client.command()
-async def reset(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    voice.stop()
-    await ctx.send("Cleared")
-
 
 client.run(os.getenv("APIKEY"))
 from keep_alive import keep_alive
