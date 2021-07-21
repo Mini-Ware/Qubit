b78759d04f667e55c4d7bff2007f76890b428566
Mini Ware <codauknow@gmail.com>
2021-07-21 21:15:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ var server=http.createServer((function(request,response)
   if (request.method == 'POST') {
         var body = '';
         request.on('data', function (data) {
-            body = data;
-            var post = JSON.stringify(body);
-            const Discord = require('discord.js');
-            const hook = new Discord.WebhookClient('866715361097416774', 'EFV5atQvs_YD3HaFQDMjc0YtBgSfNM5lgFCUk9hExipb5rhgUkR3EelRo7q5X9ovymDp');
-            hook.send(post);
+            body += data;
+        });
+        request .on('end', function() {
+          var post = JSON.stringify(body);
+          const Discord = require('discord.js');
+          const hook = new Discord.WebhookClient('866715361097416774', process.env.HOOK);
+          hook.send(post);
         });
   }else{
     response.writeHeader(200, {"Content-Type": "text/html"});
