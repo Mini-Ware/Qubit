1db05f3c639ce057c6e488eae8812fb09f16bbb1
Mini Ware <codauknow@gmail.com>
2021-08-26 23:43:27 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+var http=require('http');
+var fs = require("fs");
+var markdown = ""
+fs.readFile('index.html', function (err, data) {
+      markdown = data;
+});
+var server=http.createServer((function(request,response)
+{
+  if (request.method == 'POST') {
+        var body = '';
+        request.on('data', function (data) {
+            body += data;
+        });
+        request.on('end', function() {
+          var post = JSON.stringify(body);
+          const Discord = require('discord.js');
+          const hook = new Discord.WebhookClient('866715361097416774', process.env.HOOK);
+          hook.send(post);
+        });
+  }else{
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write(markdown);
+    response.end();
+  }
+  response.statusCode=200;
+}));
+server.listen(7000);
