d6066352956c4484986639b71ab4d3c8a2c73135
Mini-Ware <codauknow@gmail.com>
2023-06-02 01:55:34 +0800

    Include files in src folder and gitignore

---
+++ src/commands.js
@@ -0,0 +1,14 @@
+/**
+ * Share command metadata from a common spot to be used for both runtime
+ * and registration.
+ */
+
+export const NEWS_COMMAND = {
+  name: 'news',
+  description: 'fetch some articles',
+};
+
+export const HELP_COMMAND = {
+  name: 'help',
+  description: 'list all commands',
+};
---
+++ src/server.js
@@ -0,0 +1,119 @@
+/**
+ * The core server that runs on a Cloudflare worker.
+ */
+
+import { Router } from 'itty-router';
+import {
+  InteractionResponseType,
+  InteractionType,
+  verifyKey,
+} from 'discord-interactions';
+import { NEWS_COMMAND, HELP_COMMAND } from './commands.js';
+import { getArticles } from './news.js';
+import { InteractionResponseFlags } from 'discord-interactions';
+
+class JsonResponse extends Response {
+  constructor(body, init) {
+    const jsonBody = JSON.stringify(body);
+    init = init || {
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+    };
+    super(jsonBody, init);
+  }
+}
+
+const router = Router();
+
+/**
+ * A simple :wave: hello page to verify the worker is working.
+ */
+router.get('/', (request, env) => {
+  return new Response(`👋 ${env.DISCORD_APPLICATION_ID}`);
+});
+
+/**
+ * Main route for all requests sent from Discord.  All incoming messages will
+ * include a JSON payload described here:
+ * https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object
+ */
+router.post('/', async (request, env) => {
+  const { isValid, interaction } = await verifyDiscordRequest(request, env);
+  if (!isValid || !interaction) {
+    return new Response('Bad request signature.', { status: 401 });
+  }
+
+  console.log(JSON.stringify(interaction, null, 2));
+
+  if (interaction.type === InteractionType.PING) {
+    // The `PING` message is used during the initial webhook handshake, and is
+    // required to configure the webhook in the developer portal.
+    console.log('Handling Ping request');
+    return new JsonResponse({
+      type: InteractionResponseType.PONG,
+    });
+  }
+
+  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+    // Most user commands will come as `APPLICATION_COMMAND`.
+    switch (interaction.data.name.toLowerCase()) {
+      case NEWS_COMMAND.name.toLowerCase(): {
+        console.log('handling news request');
+        const articles = await getArticles();
+        return new JsonResponse({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: articles,
+          },
+        });
+      }
+      case HELP_COMMAND.name.toLowerCase(): {
+        const applicationId = env.DISCORD_APPLICATION_ID;
+        const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=${applicationId}&scope=applications.commands`;
+        return new JsonResponse({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: INVITE_URL,
+            flags: InteractionResponseFlags.EPHEMERAL,
+          },
+        });
+      }
+      default:
+        console.error('Unknown Command');
+        return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
+    }
+  }
+
+  console.error('Unknown Type');
+  return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
+});
+router.all('*', () => new Response('Not Found.', { status: 404 }));
+
+async function verifyDiscordRequest(request, env) {
+  const signature = request.headers.get('x-signature-ed25519');
+  const timestamp = request.headers.get('x-signature-timestamp');
+  const body = await request.text();
+  const isValidRequest =
+    signature &&
+    timestamp &&
+    verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY);
+  if (!isValidRequest) {
+    return { isValid: false };
+  }
+
+  return { interaction: JSON.parse(body), isValid: true };
+}
+
+export default {
+  /**
+   * Every request to a worker will start in the `fetch` method.
+   * Verify the signature with the request, and dispatch to the router.
+   * @param {*} request A Fetch Request object
+   * @param {*} env A map of key/value pairs with env vars and secrets from the cloudflare env.
+   * @returns
+   */
+  async fetch(request, env) {
+    return router.handle(request, env);
+  },
+};
